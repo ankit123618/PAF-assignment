3ba7f35a333d0bd50219319bed3c0c8ae78598d7
ankit sharma <as4808385@gmail.com>
2023-10-12 13:17:26 +0530

    fetched data from APIs

---
+++ index.php
@@ -0,0 +1,98 @@
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>संतवाणी श्रृंखला | आचार्य प्रशांत</title>
+    <link rel="shortcut icon" href="ic_favicon.png" type="image/x-icon">
+</head>
+<body>
+    <div class="container-fluid header pt-3 pl-5 text-white">
+        <img src="ic_favicon.png" alt="logo" class="logo">
+        <div class="nav-links-top">
+            <a href="#">home</a>
+            <a href="#">live sessions</a>
+            <a href="#">video series</a>
+            <a href="#">ap books</a>
+            <a href="#">ap articles</a>
+            <a href="#">invite</a>
+            <a href="#">in media</a>
+            <a href="#">donate</a>
+        </div>
+    </div>
+    <div class="header-2">
+        <p>get access to all 350+ video series at once <a href="#">click here</a></p>
+    </div>
+    <div class="container-fluid pl-2 section" id="section1">
+        <h2 id="course-title"></h2>
+        <h3 id="course-subtitle"></h3>
+        <p id="course-description"></p>
+    </div>
+    <div class="section2" id="video-series">
+        
+    </div>
+    <div class="section3" id="other">
+        <h2 class="container-fluid">other helpful video series</h2>
+    </div>
+    <div class="section4" id="last-section">
+        <div class="support">
+            <h3>FAQs</h3>
+            <p>Can’t find the answer you’re looking for? Reach out to our <a href="#">support</a>team</p>
+        </div>
+        <div class="FAQ" id="faq">
+
+        </div>
+    </div>
+    <div class="footer">
+        <div>
+            <h2>live events</h2>
+            <ul>
+                <li>bhagwad gita</li>
+                <li>sant sarita</li>
+                <li>ashtavakra samhita</li>
+                <li>Bodh Pratyusha</li>
+            </ul>
+        </div>
+        <div>
+        <h2>wisdom content</h2>
+            <ul>
+                <li>video series</li>
+                <li>AP books</li>
+                <li>AP circle</li>
+                <li>AP articles</li>
+            </ul>
+        </div>
+        <div>
+        <h2>more</h2>
+            <ul>
+                <li>about acharya prashant</li>
+                <li>Invite Him</li>
+                <li>Interview Him</li>
+                <li>Ghar Ghar Upanishad</li>
+                <li>Media and Public Interaction</li>
+            </ul>
+        </div>
+        <div>
+        <h2>social media</h2>
+            <ul>
+                <li>For English</li>
+                <li>For Hindi</li>
+            </ul> 
+        </div>
+        <div>
+            <h2>download app</h2>
+        </div>
+        <div>
+            <h2>contact us</h2>
+            <ul>
+                <li>support@advait.org.in</li>
+                <li>+91 9650585100</li>
+                <li>+91 9650585100</li>
+            </ul>
+        </div>
+        <div class="container-fluid">
+            <p>Copyright © 2022 PrashantAdvait Foundation</p>
+            <p>Terms & Conditions | Privacy Policy</p>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="api.js"></script>
+</body>
